2d7c03fd0af3bc84736fefd94c74ac7810932633
Vlad Tarasenko <tarasenkovlady@gmail.com>
2017-02-22 00:19:52 +0200

    HW 1-2 commit
    HW 1-2 commit

--- JS_Homework 1-2/js/Math_pow.js
+++ JS_Homework 1-2/js/Math_pow.js
@@ -1,11 +1,31 @@
+//
+//
+// function mathPow() {
+//   var base = prompt('число');
+//   var exponent = prompt('степень');
+//   var result = Math.pow(base, exponent);
+//   // alert(result);
+//   console.log(result);
+// }
+//
+// mathPow();
 
+function pow(a, b) {
+  var powResult = a;
 
-function mathPow() {
-  var base = prompt('число');
-  var exponent = prompt('степень');
-  var result = Math.pow(base, exponent);
-  // alert(result);
-  console.log(result);
+  for (var i = 1; i < b; i++) {
+    powResult = powResult*a;
+  }
+
+  return powResult;
 }
 
-mathPow();
+var a = prompt("введите число", '');
+var b = prompt("введите степень", '');
+
+if (b < 1 ){
+  alert('введите значение степени больше 1')
+  location.reload(); //функция перезагружает страницу
+} else {
+  alert( pow(a, b) );
+}
